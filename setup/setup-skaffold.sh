43e1024f70c2759c367f10db4b768579f6609682
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2021-01-17 15:37:44 -0300

    Add configurable version for Skaffold

--- setup/setup-skaffold.sh
+++ setup/setup-skaffold.sh
@@ -3,6 +3,7 @@
 set -e
 
 # Install Skaffold
-curl --location --output skaffold --silent "https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64"
+SKAFFOLD_INSTALL_VERSION=${SKAFFOLD_VERSION:-latest}
+curl --location --output skaffold --silent "https://storage.googleapis.com/skaffold/releases/${SKAFFOLD_INSTALL_VERSION}/skaffold-linux-amd64"
 sudo install skaffold /usr/local/bin/
 sudo rm ./skaffold
