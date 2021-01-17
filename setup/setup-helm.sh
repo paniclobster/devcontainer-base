3e266572d1eae00cbab0767f2a50b254d9c7904d
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2021-01-17 15:37:31 -0300

    Add configurable version for Helm

--- setup/setup-helm.sh
+++ setup/setup-helm.sh
@@ -3,4 +3,5 @@
 set -e
 
 # Install Helm
-curl --location --silent "https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3" | sudo bash -
+HELM_INSTALL_VERSION=${HELM_VERSION:-master}
+curl --location --silent "https://raw.githubusercontent.com/helm/helm/${HELM_INSTALL_VERSION}/scripts/get-helm-3" | sudo bash -
