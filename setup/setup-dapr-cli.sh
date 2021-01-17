daa5e89222ee6cf6b8adf7b845eac322596eee98
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2021-01-17 15:37:27 -0300

    Add configurable version for Dapr CLI

--- setup/setup-dapr-cli.sh
+++ setup/setup-dapr-cli.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-# Install Helm
-wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+# Install Dapr CLI
+DAPR_CLI_INSTALL_VERSION=${DAPR_CLI_VERSION:-master}
+curl --location --silent "https://raw.githubusercontent.com/dapr/cli/${DAPR_CLI_INSTALL_VERSION}/install/install.sh" | sudo bash -
