800427da2e6f59dc9ba0b4199ce1283c2ed2e582
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2021-01-18 14:02:43 -0300

    Add symlinks to python3 setup

--- setup/setup-python3.sh
+++ setup/setup-python3.sh
@@ -8,3 +8,9 @@ sudo apk add --no-cache \
 
 # Install pip
 curl  https://bootstrap.pypa.io/get-pip.py | sudo python3
+
+# Make symlinks that are expected to exist
+sudo ln -s /usr/local/binidle3 /usr/local/binidle
+sudo ln -s /usr/local/binpydoc3 /usr/local/binpydoc
+sudo ln -s /usr/local/binpython3 /usr/local/binpython
+sudo ln -s /usr/local/binpython3-config /usr/local/binpython-config
