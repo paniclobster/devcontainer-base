1649f2390383b944f213cfa9816101b53b098443
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2021-05-24 23:37:22 -0300

    Add packer installation

---
+++ alpine/setup/setup-packer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Install packer
+PACKER_INSTALL_VERSION=$(curl --location --silent "https://checkpoint-api.hashicorp.com/v1/check/packer" | jq -r ".current_version")
+curl --location --output packer.zip --silent "https://releases.hashicorp.com/packer/${PACKER_INSTALL_VERSION}/packer_${PACKER_INSTALL_VERSION}_linux_amd64.zip"
+unzip packer.zip
+sudo install packer /usr/local/bin/
+sudo rm ./packer ./packer.zip
