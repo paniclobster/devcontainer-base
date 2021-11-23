fb6701d3711d698a1eb13a3136b7712bd34b82e9
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2021-11-23 10:56:14 -0300

    feat: update base images
    Update Alpine base image to Microsoft's provided devcontainer image.
    Update Debian base image to Microsoft's provided devcontainer image.
    
    BREAKING CHANGE: the new base images may prevent some of the setups or
    settings to work properly

--- alpine/scripts/devcontainer-setup.sh
+++ alpine/scripts/devcontainer-setup.sh
@@ -3,5 +3,5 @@
 set -e
 
 if [ ! -f "${DEVCONTAINER_SETUP_DIR}/setup-${1}.log" ]; then
-    exec "${DEVCONTAINER_SETUP_DIR}/setup-${1}.sh" ${@:2} | sudo tee "${DEVCONTAINER_SETUP_DIR}/setup-${1}.log" >>/dev/null
+    exec "${DEVCONTAINER_SETUP_DIR}/setup-${1}.sh" ${@:2} 2>&1 | sudo tee "${DEVCONTAINER_SETUP_DIR}/setup-${1}.log"
 fi
