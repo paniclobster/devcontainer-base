60cc5ec46923fcdc3448d619d75bb95209588eeb
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2022-07-14 10:23:38 -0300

    chore: fix comment line typo

--- alpine/setup/setup-tilt.sh
+++ alpine/setup/setup-tilt.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# Install kind
+# Install tilt
 curl --silent "https://api.github.com/repos/tilt-dev/tilt/releases" | grep browser_download | grep linux | cut -d '"' -f 4 | grep x86_64 | sort | tail -n 1 | xargs curl --location --output tilt.tar.gz --silent
 tar -xvf tilt.tar.gz
 sudo install tilt /usr/local/bin/
