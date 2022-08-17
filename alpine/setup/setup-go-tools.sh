aaa5a3366d31ab973139e818fc0429c38c5ff08f
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2022-08-17 11:56:24 -0300

    chore: update go tools setup

--- alpine/setup/setup-go-tools.sh
+++ alpine/setup/setup-go-tools.sh
@@ -4,27 +4,10 @@ set -ex
 
 # Install Go tools
 export GOPATH=${DOCKER_GO_PATH}
-go get -x -d github.com/stamblerre/gocode
-go build -o gocode-gomod github.com/stamblerre/gocode
-mv gocode-gomod "${GOPATH}/bin/"
-go get -u -v \
-    github.com/acroca/go-symbols \
-    github.com/cweill/gotests/... \
-    github.com/davidrjenni/reftools/cmd/fillstruct \
-    github.com/fatih/gomodifytags \
-    github.com/go-delve/delve/cmd/dlv \
-    github.com/godoctor/godoctor \
-    github.com/haya14busa/goplay/cmd/goplay \
-    github.com/josharian/impl \
-    github.com/mdempsky/gocode \
-    github.com/ramya-rao-a/go-outline \
-    github.com/rogpeppe/godef \
-    github.com/sqs/goreturns \
-    github.com/stamblerre/gocode \
-    github.com/uudashr/gopkgs/v2/cmd/gopkgs \
-    github.com/uudashr/gopkgs/v2/cmd/gopkgs \
-    golang.org/x/lint/golint \
-    golang.org/x/tools/cmd/goimports \
-    golang.org/x/tools/cmd/gorename \
-    golang.org/x/tools/cmd/guru
+go install -v github.com/cweill/gotests/gotests@latest
+go install -v github.com/fatih/gomodifytags@latest
+go install -v github.com/go-delve/delve/cmd/dlv@latest
+go install -v github.com/haya14busa/goplay/cmd/goplay@latest
+go install -v github.com/josharian/impl@latest
 go install -v golang.org/x/tools/gopls@latest
+go install -v honnef.co/go/tools/cmd/staticcheck@latest
